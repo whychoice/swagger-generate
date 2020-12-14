966837380dffc466dd78d8b8c85bb9f9d013f6c3
GitHub Action <action@github.com>
2020-12-14 05:56:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/whychoice/swaggergenerate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/whychoice/swaggergenerate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.whychoice.swaggergenerate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.whychoice.swaggergenerate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/whychoice/swaggergenerate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/whychoice/swaggergenerate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.whychoice.swaggergenerate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.whychoice.swaggergenerate.MyBundle
 
 class MyApplicationService {
 
